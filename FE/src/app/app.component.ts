f9f0b02deb46d57b34d6556876dd266fbecfffee
TomekStolarz <tomekstolarz0@gmail.com>
2024-10-22 20:59:37 +0200

    Configurate eslint and prettier

--- FE/src/app/app.component.ts
+++ FE/src/app/app.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'ww-root',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'FE';
