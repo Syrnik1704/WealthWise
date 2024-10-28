c018728254b977f9d24d0da6f44060ec36a8b1cb
TomekStolarz <tomekstolarz0@gmail.com>
2024-10-28 20:31:51 +0100

    -[feat] added login component

---
+++ FE/src/login/componenets/login.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ww-login',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      login works!
+    </p>
+  `,
+  styles: ``
+})
+export class LoginComponent {
+
+}
