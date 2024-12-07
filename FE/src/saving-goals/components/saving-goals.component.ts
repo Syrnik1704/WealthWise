57ff54d15f8e0020000be19eb85e4fdf9ff34eab
TomekStolarz <tomekstolarz0@gmail.com>
2024-12-07 14:59:57 +0100

    added saving goals

--- FE/src/saving-goals/components/saving-goals.component.ts
+++ FE/src/saving-goals/components/saving-goals.component.ts
@@ -3,15 +3,9 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 @Component({
   selector: 'ww-saving-goals',
   standalone: true,
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [],
-  template: `
-    <p>
-      saving-goals works!
-    </p>
-  `,
-  styles: ``
+  template: ` <p>saving-goals works!</p> `,
+  styles: ``,
 })
-export class SavingGoalsComponent {
-
-}
+export class SavingGoalsComponent {}
