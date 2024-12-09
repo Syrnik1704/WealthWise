13276ff6c2b0ff5f4cff118bede4204e83edf2ba
Konrad <syrnikkonrad@gmail.com>
2024-12-09 15:17:16 +0100

    Add sending welcome mail after signing up

--- src/main/java/com/example/wealthwise_api/Controller/AuthenticationController.java
+++ src/main/java/com/example/wealthwise_api/Controller/AuthenticationController.java
@@ -4,11 +4,11 @@ import com.example.wealthwise_api.DTO.AuthenticationRequest;
 import com.example.wealthwise_api.DTO.AuthenticationRequestToken;
 import com.example.wealthwise_api.DTO.UserRegistrationRequest;
 import com.example.wealthwise_api.Services.AuthenticationService;
+import com.example.wealthwise_api.Services.EmailService;
 import com.example.wealthwise_api.Services.RegistrationService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -18,10 +18,12 @@ public class AuthenticationController {
 
     private final AuthenticationService authenticationService;
     private final RegistrationService registrationService;
+    private final EmailService emailService;
 
-    public AuthenticationController(AuthenticationService authenticationService, RegistrationService registrationService) {
+    public AuthenticationController(AuthenticationService authenticationService, RegistrationService registrationService, EmailService emailService) {
         this.authenticationService = authenticationService;
         this.registrationService = registrationService;
+        this.emailService = emailService;
     }
 
     @PostMapping(value = "/login",produces = MediaType.APPLICATION_JSON_VALUE)
@@ -31,7 +33,11 @@ public class AuthenticationController {
 
     @PostMapping(value = "/register", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> register(@RequestBody UserRegistrationRequest request){
-        return registrationService.register(request);
+        ResponseEntity<Object> responseEntity = registrationService.register(request);
+        if (responseEntity.getStatusCode() == HttpStatus.OK){
+            emailService.sendEmail(request.email(), "Welcome in WealthWise Application!", "static/email_welcome_notification.html");
+        }
+        return responseEntity;
     }
 
     @PostMapping(value = "refreshToken", produces = MediaType.APPLICATION_JSON_VALUE)
