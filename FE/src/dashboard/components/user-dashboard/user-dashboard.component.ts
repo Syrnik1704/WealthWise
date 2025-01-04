772534ede05b65fe1ac21bd433b2b46abf3d8a93
TomekStolarz <tomekstolarz0@gmail.com>
2025-01-04 23:43:36 +0100

    goal preview

--- FE/src/dashboard/components/user-dashboard/user-dashboard.component.ts
+++ FE/src/dashboard/components/user-dashboard/user-dashboard.component.ts
@@ -1,11 +1,59 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  DestroyRef,
+  inject,
+  OnInit,
+} from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { TranslateModule } from '@ngx-translate/core';
+import { Store } from '@ngxs/store';
+import { tap } from 'rxjs';
+import { SavingGoal } from '../../../saving-goals/models';
+import { SavingGoalApiService } from '../../../saving-goals/services';
+import { UserSelectors } from '../../../shared';
 
 @Component({
   selector: 'ww-user-dashboard',
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [],
+  imports: [TranslateModule, MatIconModule, MatProgressBarModule, CommonModule, MatCardModule],
   templateUrl: './user-dashboard.component.html',
   styleUrl: './user-dashboard.component.scss',
 })
-export class UserDashboardComponent {}
+export class UserDashboardComponent implements OnInit {
+  private readonly store = inject(Store);
+  private readonly savingGoalApiService = inject(SavingGoalApiService);
+  private readonly destroyRef = inject(DestroyRef);
+  private readonly cdRef = inject(ChangeDetectorRef);
+  protected user = this.store.selectSnapshot(UserSelectors.user);
+  protected incomingTargets: SavingGoal[] = [];
+  protected mostFulfilledTargets: SavingGoal[] = [];
+
+  public ngOnInit(): void {
+    this.savingGoalApiService
+      .getGoalList()
+      .pipe(
+        tap(goals => {
+          this.incomingTargets = goals.slice(0, 3);
+          this.mostFulfilledTargets = goals
+            .filter(e => e.targetAmount > e.currentAmount)
+            .sort((a, b) => {
+              if (a.progress === undefined || b.progress === undefined) {
+                return 0;
+              }
+              return -(a.progress - b.progress);
+            })
+            .slice(0, 3);
+          this.cdRef.detectChanges();
+        }),
+        takeUntilDestroyed(this.destroyRef)
+      )
+      .subscribe();
+  }
+}
