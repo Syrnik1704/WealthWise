815ef693374aa9bac56c6bbb648cabd5c70e2785
Konrad <syrnikkonrad@gmail.com>
2025-01-09 19:02:22 +0100

    Add get user total balance endpoint

--- src/main/java/com/example/wealthwise_api/Controller/UserController.java
+++ src/main/java/com/example/wealthwise_api/Controller/UserController.java
@@ -3,7 +3,9 @@ package com.example.wealthwise_api.Controller;
 
 import com.example.wealthwise_api.DTO.ChangePasswordRequest;
 import com.example.wealthwise_api.DTO.TokenRequest;
+import com.example.wealthwise_api.Entity.Expenses;
 import com.example.wealthwise_api.Services.ChangePasswordService;
+import com.example.wealthwise_api.Services.IncomesService;
 import com.example.wealthwise_api.Services.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.MediaType;
@@ -17,17 +19,21 @@ import org.springframework.security.access.prepost.PreAuthorize;
 public class UserController {
 
     private ChangePasswordService changePasswordService;
+    private IncomesService incomesService;
 
-    private UserService userService;
-
-    public UserController(ChangePasswordService changePasswordService, UserService userService) {
-        this.userService = userService;
+    public UserController(ChangePasswordService changePasswordService, IncomesService incomesService) {
+        this.incomesService = incomesService;
         this.changePasswordService = changePasswordService;
     }
 
     @PostMapping(value="/changePassword", produces = MediaType.APPLICATION_JSON_VALUE )
     public ResponseEntity<?> changePassword(@RequestBody ChangePasswordRequest request){
         return changePasswordService.changePassword(request);
+    }
+
+    @GetMapping("/balance")
+    public ResponseEntity<?> getUserBalance(HttpServletRequest request) {
+        return incomesService.getUserBalance(request);
     }
 
 }
